21b1eedc9bfc024a8047b2dd2bec8079d673a92f
rob <rob@rgordon.co.uk>
2018-08-17 19:07:32 +0100

    Separate from the oddjob module.

---
+++ src/test/java/org/oddjob/launch/PathParserTest.java
@@ -0,0 +1,126 @@
+package org.oddjob.launch;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PathParserTest {
+
+	@Rule public TestName name = new TestName();
+
+	public String getName() {
+        return name.getMethodName();
+    }
+
+	private static final Logger logger = LoggerFactory.getLogger(
+			PathParserTest.class);
+	
+	String pathToParse = null;
+	String result1 = null;
+	String result2 = null;
+	
+    @Before
+    public void setUp() throws Exception {
+
+		logger.info("-----------------" + getName() + "-----------");
+	}
+	
+	void pathSetUp() {
+		
+		if (";".equals(File.pathSeparator)) {
+			logger.info("Windows style path.");
+			pathToParse = "c:\\fruit\\apple.jar;c:\\fruit\\orange.jar";
+			result1 = "c:\\fruit\\apple.jar";
+			result2 = "c:\\fruit\\orange.jar";
+		} 
+		else if (":".equals(File.pathSeparator)) {
+			logger.info("Unix style path.");
+			pathToParse = "/local/lib/fruit/apple.jar:/local/lib/fruit/orange.jar";
+			result1 = "/local/lib/fruit/apple.jar";
+			result2 = "/local/lib/fruit/orange.jar";
+		}
+		else {
+			logger.info("Unknown path style. Test will just succeed.");
+		}
+	}
+	
+	
+   @Test
+	public void testPathParse() {
+		pathSetUp();
+		
+		if (pathToParse == null) {
+			return;
+		}
+		
+		PathParser test = new PathParser();
+		
+		String[] after = test.processArgs(
+				new String[] { 
+						"before", 
+						"-cp", 
+						pathToParse,
+						"after"});
+		
+		assertEquals(2, after.length);
+		
+		assertEquals("before", after[0]);
+		assertEquals("after", after[1]);
+		
+		String[] results = test.getElements();
+		
+		assertEquals(2, results.length);
+		
+		assertEquals(result1, results[0]);
+		assertEquals(result2, results[1]);
+	}
+	
+   @Test
+	public void testArgButNoPath() {
+		
+		PathParser test = new PathParser();
+		
+		try {
+			test.processArgs(new String[] { 
+					"before", "-cp" });
+			fail("Should fail.");
+		}
+		catch (IllegalArgumentException e) {
+			// expected
+		}
+	}
+	
+   @Test
+	public void testContinue() {
+		
+		PathParser test = new PathParser();
+		
+		String[] after =  test.processArgs(new String[] { 
+					"--", "-cp" });
+		
+		assertEquals("--", after[0]);
+		assertEquals("-cp", after[1]);
+		
+		assertEquals(2, after.length);
+	}
+	
+   @Test
+	public void testTwoClassPathsContinue() {
+		
+		PathParser test = new PathParser();
+		
+		String[] after =  test.processArgs(new String[] { 
+					"-classpath", "foo", "-cp" });
+		
+		assertEquals("-cp", after[0]);
+		
+		assertEquals(1, after.length);
+	}
+}
---
+++ src/test/java/org/oddjob/launch/SystemPropertyArgParserTest.java
@@ -0,0 +1,55 @@
+package org.oddjob.launch;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SystemPropertyArgParserTest {
+
+   @Test
+	public void testArgs() {
+		
+		SystemPropertyArgParser test = new SystemPropertyArgParser();
+		
+		String before = System.getProperty("favourite.fruit");
+		
+		String[] after = test.processArgs(new String[] { 
+				"-Dfavourite.fruit=", "-Dfavourite.fruit=apples"
+		});
+		
+		assertEquals(0, after.length);
+		
+		assertEquals("apples", System.getProperty("favourite.fruit"));
+		
+		if (before == null) {
+			System.getProperties().remove("favourite.fruit");
+		}
+		else {
+			System.setProperty("favourite.fruit", before);
+		}
+	}
+	
+   @Test
+	public void testContinue() {
+		
+		SystemPropertyArgParser test = new SystemPropertyArgParser();
+		
+		String[] after = test.processArgs(new String[] { 
+				"--", "-Dfavourite.fruit=apples"
+		});
+		
+		assertEquals(2, after.length);
+	}
+	
+   @Test
+	public void testNoneMatches() {
+		
+		SystemPropertyArgParser test = new SystemPropertyArgParser();
+		
+		String[] after = test.processArgs(new String[] { 
+				"-D", "-D=apples"
+		});
+		
+		assertEquals(2, after.length);
+	}
+}
