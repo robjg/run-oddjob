5dd627952f84affde9515945c4b2642546eb3bf3
Rob Gordon <rob@rgordon.co.uk>
2022-06-28 19:19:08 +0100

    Add stop method to support Apache Procrun.

--- src/main/java/org/oddjob/launch/Launcher.java
+++ src/main/java/org/oddjob/launch/Launcher.java
@@ -21,140 +21,171 @@ import java.util.Objects;
  *   <li>Any jars in the opt/lib directory.</li>
  *   <li>The opt/classes directory.</li>
  * </ul>
- * 
+ *
  * @author Rob Gordon, Based on Ant.
  */
 public class Launcher {
 
-	public static final String ODDJOB_HOME_PROPERTY = "oddjob.home";
-	
-	public static final String ODDJOB_RUN_JAR_PROPERTY = "oddjob.run.jar";
-	
+    public static final String ODDJOB_HOME_PROPERTY = "oddjob.home";
+
+    public static final String ODDJOB_RUN_JAR_PROPERTY = "oddjob.run.jar";
+
     public static final String ODDJOB_MAIN_CLASS = "org.oddjob.Main";
 
-    /** The class loader to find the main class in. */
-	private final ClassLoader classLoader;
+    public static final String MAIN_METHOD = "main";
 
-	/** The name of the class that contains the main method. */
-	private final String className;
-	
-	/** The arguments to pass to main. */
-	private final String[] args;
+    public static final String STOP_METHOD = "stop";
 
-	public Launcher(ClassLoader classLoader, String className, String[] args) {
-		Objects.requireNonNull(classLoader);
-		Objects.requireNonNull(className);
+    private static volatile AutoCloseable stop;
 
-		this.classLoader = classLoader;
-		this.className = className;
-		this.args = args;
-	}
+    /**
+     * The class loader to find the main class in.
+     */
+    private final ClassLoader classLoader;
 
-	public void launch() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-    	
+    /**
+     * The name of the class that contains the main method.
+     */
+    private final String className;
 
-    	ClassLoader currentLoader = Thread.currentThread().getContextClassLoader();
+    /**
+     * The arguments to pass to main.
+     */
+    private final String[] args;
+
+    public Launcher(ClassLoader classLoader, String className, String[] args) {
+        Objects.requireNonNull(classLoader);
+        Objects.requireNonNull(className);
+
+        this.classLoader = classLoader;
+        this.className = className;
+        this.args = args;
+    }
+
+    public void launch() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        ClassLoader currentLoader = Thread.currentThread().getContextClassLoader();
         Thread.currentThread().setContextClassLoader(classLoader);
         try {
             Class<?> mainClass = classLoader.loadClass(className);
-            
+
             // use reflection because main is now in another
-            // class loader space so we can't get at it.
-            Method method = mainClass.getMethod("main",
-					String[].class);
-            
-            method.invoke(null, (Object) args);
-            
+            // class loader space, so we can't get at it.
+            Method main = mainClass.getMethod(MAIN_METHOD,
+                    String[].class);
+
+            Method stop;
+            try {
+                stop = mainClass.getMethod(STOP_METHOD);
+            }
+            catch (NoSuchMethodException e) {
+                // ignore. stop method optional.
+                stop = null;
+            }
+
+            if (stop == null) {
+                Launcher.stop = () -> {
+                    throw new UnsupportedOperationException("No stop method");
+                };
+            }
+            else {
+                Method finalStop = stop;
+                Launcher.stop = () -> finalStop.invoke(null);
+            }
+
+            main.invoke(null, (Object) args);
+
         } finally {
-            Thread.currentThread().setContextClassLoader(currentLoader);        	
+            Thread.currentThread().setContextClassLoader(currentLoader);
         }
     }
 
     /**
      * Provides the Oddjob class loader.
-     * 
+     *
      * @param currentLoader The current classloader that becomes the parent
-     * @param classpath The classpath to add to the class loader.
-	 *
+     * @param classpath     The classpath to add to the class loader.
      * @return A class loader.
-	 *
      * @throws IOException If Failing to set canonical paths.
      */
-	static ClassLoader getClassLoader(ClassLoader currentLoader, String[] classpath) 
-	throws IOException {
-		
+    static ClassLoader getClassLoader(ClassLoader currentLoader, String[] classpath)
+            throws IOException {
+
         File sourceJar = Locator.getClassSource(Launcher.class);
-        File jarDir = sourceJar.getParentFile();        
-        
+        File jarDir = sourceJar.getParentFile();
+
         System.setProperty(ODDJOB_HOME_PROPERTY, jarDir.getCanonicalPath());
         System.setProperty(ODDJOB_RUN_JAR_PROPERTY, sourceJar.getCanonicalPath());
-                
+
         List<File> classPathList = new ArrayList<>();
-        
+
         // add the source jar
         classPathList.add(sourceJar);
-        
+
         // expand the classpath entries.
         for (String entry : classpath) {
-        	File[] entryFiles = new FileSpec(
-            		new File(entry)).getFiles();
-        	classPathList.addAll(Arrays.asList(entryFiles));
+            File[] entryFiles = new FileSpec(
+                    new File(entry)).getFiles();
+            classPathList.addAll(Arrays.asList(entryFiles));
         }
-            
+
         // expand the lib directory
-    	File[] libFiles = new FileSpec(
-    		new File(new File(jarDir,"lib"), "*.jar")).getFiles();
-    	classPathList.addAll(Arrays.asList(libFiles));
-    	
-    	// add opt/classes
-    	classPathList.add(new File(jarDir, "opt/classes"));
-    	
-    	// expand the opt/lib directory
+        File[] libFiles = new FileSpec(
+                new File(new File(jarDir, "lib"), "*.jar")).getFiles();
+        classPathList.addAll(Arrays.asList(libFiles));
+
+        // add opt/classes
+        classPathList.add(new File(jarDir, "opt/classes"));
+
+        // expand the opt/lib directory
         File[] optFiles = new FileSpec(new File(
-        		new File(jarDir,"opt/lib"), "*.jar")).getFiles();
-    	classPathList.addAll(Arrays.asList(optFiles));
-        
-    	// The full class path
-    	ClassPathHelper classPathHelper = new ClassPathHelper(
-    			classPathList.toArray(new File[0]));
-        
+                new File(jarDir, "opt/lib"), "*.jar")).getFiles();
+        classPathList.addAll(Arrays.asList(optFiles));
+
+        // The full class path
+        ClassPathHelper classPathHelper = new ClassPathHelper(
+                classPathList.toArray(new File[0]));
+
         URL[] urls = classPathHelper.toURLs();
         classPathHelper.appendToJavaClassPath();
         final String classPath = classPathHelper.toString();
 
-		return new URLClassLoader(urls, currentLoader) {
-			@Override
-			public String toString() {
-				return "Oddjob Launcher ClassLoader: " +
-							classPath;
-			}
-		};
-	}	
+        return new URLClassLoader(urls, currentLoader) {
+            @Override
+            public String toString() {
+                return "Oddjob Launcher ClassLoader: " +
+                        classPath;
+            }
+        };
+    }
 
-	/**
-	 * Main method for launching Oddjob in it's own class loader.
-	 * The parent class loader will be taken to be the current threads
-	 * context class loader.
-	 * 
-	 * @param args Args passed to oddjob.
-	 *
-	 */
+    /**
+     * Main method for launching Oddjob in it's own class loader.
+     * The parent class loader will be taken to be the current threads
+     * context class loader.
+     *
+     * @param args Args passed to oddjob.
+     */
     public static void main(String... args) throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
 
-    	// process -D property definitions
-    	args = new SystemPropertyArgParser().processArgs(args);
-    	
-    	// process class path
-    	PathParser path = new PathParser();
-    	args = path.processArgs(args);
-    	
-        ClassLoader loader = getClassLoader(
-        		Thread.currentThread().getContextClassLoader(), 
-        		path.getElements());
+        // process -D property definitions
+        args = new SystemPropertyArgParser().processArgs(args);
 
-    	Launcher launcher = new Launcher(loader, ODDJOB_MAIN_CLASS, args);
-    	
-    	launcher.launch();
+        // process class path
+        PathParser path = new PathParser();
+        args = path.processArgs(args);
+
+        ClassLoader loader = getClassLoader(
+                Thread.currentThread().getContextClassLoader(),
+                path.getElements());
+
+        Launcher launcher = new Launcher(loader, ODDJOB_MAIN_CLASS, args);
+
+        launcher.launch();
     }
-    
+
+    public static void stop(String... args) throws Exception {
+        Objects.requireNonNull(stop, "Launcher not running.")
+                .close();
+    }
 }
